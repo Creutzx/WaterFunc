926f9e16c72c6e73f17bb6b0117b12f33149b449
CREUTZBURG Brian <bcreutz@deq.state.or.us>
2018-02-08 13:57:50 -0800

    creates map showing variation in an environmental variable within a watershed

---
+++ WaterFunc.py
@@ -0,0 +1,30 @@
+
+
+"""uses a basemap, adds a layer file with the environmental characteristic  that is sought"""
+
+
+def waterfunc(lyr_name):
+    import arcpy
+    import os
+
+    str_dir_main = r"\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps"
+    str_mxd_blank = r"Upper_Yaquina_TMDL_Blank_ME.mxd"
+    str_df_cur = r"Layers"
+    str_mxd_save_pre = r"Upper_Yaquina_TMDL_UY_"
+    str_mxd_save_post = r".mxd"
+    lyr_save_pre = r"UY_"
+    lyr_save_post = r".lyr"
+    lyr_name = self.lyr_name
+
+    mxd = arcpy.mapping.MapDocument(str_dir_main + "\\" + str_mxd_blank)
+    df = arcpy.mapping.ListDataFrames(mxd, str_df_cur)[0]
+    str_layerfile = os.path.join(str_dir_main, lyr_save_pre + lyr_name + lyr_save_post)
+    addlayer = arcpy.mapping.Layer(str_layerfile)
+    arcpy.mapping.AddLayer(df, addlayer, "BOTTOM")
+    arcpy.RefreshTOC()
+    arcpy.RefreshActiveView()
+    mxd.saveACopy(str_dir_main + "\\" + str_mxd_save_pre + lyr_name + str_mxd_save_post)
+
+    del addlayer, df, mxd
+
+waterfunc(lyr_name = Geo)
